d55599dd165565cd976b6db215fd2314f5038706
TryaNever <alexispernette@outlook.fr>
2025-11-21 11:25:36 +0100

    Test Button Local

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button>Cliquez Ici</button>;
+};
---
+++ src/components/button/Button.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from "./Button";
+
+test("Le button affiche le texte 'Cliquez Ici'", () => {
+  render(<Button />);
+  const expectedText = screen.getByText("Cliquez Ici");
+  expect(expectedText).toBeInTheDocument();
+});
